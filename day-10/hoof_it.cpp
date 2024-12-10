a14de7f286e81e5e4ed79307ab4bbb0eca0762a5
Yashraj <yashsharmau9@gmail.com>
2024-12-10 19:06:29 +0530

    added both solutions

--- day-10/hoof_it.cpp
+++ day-10/hoof_it.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n =52;
+int n = 52;
 int main() {
 #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
@@ -27,8 +27,10 @@ int main() {
     vector<int> dx = {0,1,0,-1};
     vector<int> dy = {-1,0,1,0};
     for(int i=0;i<pos.size();i++) {
+
             int row = pos[i].first;
             int col = pos[i].second;
+
             queue<pair<int,int>> q;
             q.push({row,col});
             vector<vector<int>> vis(n,vector<int>(n,0));
@@ -37,26 +39,18 @@ int main() {
                 int r = q.front().first;
                 int c = q.front().second;
                 q.pop();
+
                 if(arr[r][c] == '9') {
                     score++;
                     continue;
                 }
+
                 for(int i=0;i<4;i++){
                     int nrow = r + dx[i];
                     int ncol = c + dy[i];
                     if(nrow>=0 && ncol>=0 && nrow<n && ncol<n && arr[nrow][ncol]==arr[r][c]+1 && !vis[nrow][ncol]) {
                         q.push({nrow,ncol});
-                        int c = 0;
-                        for(int k=0;k<4;k++) {
-                            int nn = nrow + dx[k];
-                            int nm = ncol + dy[k];
-                            if(nn>=0 && nn<n && nm>=0 && nm<n && arr[nn][nm]==arr[nrow][ncol]-1) {
-                                c++;
-                            }
-                        }
-                        if(c==0) {
-                            vis[nrow][ncol] = 1;
-                        }
+                        vis[nrow][ncol] = 1;
                     }
                 }
             }
